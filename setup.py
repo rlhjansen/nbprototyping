eda67d52a054a10d0fe87078197454f95191144a
rlhjansen <rlh.jansen@outlook.com>
2023-06-29 13:02:00 +0200

    prep for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='notebook_prototyping',
+    version='0.1.1',
+    license='MIT',
+    author="Reitze Jansen",
+    author_email='rlh.jansen@outlook.com',
+    # packages=find_packages('src'),
+    # package_dir={'': 'src'},
+    url='https://github.com/rlhjansen/nbprototyping',
+    keywords='jupyter notebook tooling',
+)
